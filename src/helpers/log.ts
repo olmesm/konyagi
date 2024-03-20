7dfb6e9fe4ef7778b4cc64522480e9dea9009261
Oliver Smit <16165237+olmesm@users.noreply.github.com>
2024-03-20 21:36:36 +0000

    solves #3

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -1,7 +1,7 @@
 import { Job } from "../types.js";
 import { deepLog, log as uLog } from "../utils/log.js";
 
-export const log: Job<any> = (s) => {
-  uLog.dev(deepLog(s, 10));
+export const kLog: Job<any> = (s) => {
+  uLog.info(deepLog(s, 10));
   return [[], s];
 };
